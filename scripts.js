da803bcd4ab08017fef8a42ff08a9e23796c94a8
Max Garber <max.garber+dev@gmail.com>
2017-05-05 19:51:52 -0400

    added skeleton javascript file

---
+++ scripts.js
@@ -0,0 +1,13 @@
+//	RiverStatusBoard: scripts.js
+//	more header stuff here later
+//	by Max Garber <max.garber+dev@gmail.com>
+
+//	initial setup
+window.onload = function () {
+	//	do setup
+};
+
+//	more functions here
+
+
+// EOF
